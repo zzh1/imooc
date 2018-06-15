83ec15089c32987a32d501f799c19e4b3bd4e261
Zhonghao_Zhao <zhaozhonghao@simuwang.com>
2018-06-15 16:14:47 +0800

    composer加载

--- index.php
+++ index.php
@@ -9,13 +9,23 @@ define('IMOOC',realpath('./'));
 define('CORE',IMOOC.'/core');
 define('APP',IMOOC.'/app');
 define('MODULE','app');
-
 define('DEBUG',true);
+
+include "vendor/autoload.php";
+
 if (DEBUG){
+    $whoops = new \Whoops\Run;
+    $errorTitle = '框架出错了';
+    $option = new \Whoops\Handler\PrettyPageHandler();
+    $option->setPageTitle($errorTitle);
+    $whoops->pushHandler($option);
+    $whoops->register();
     ini_set('display_error','On');
 }else{
     ini_set('display_error','Off');
 }
+
+dump($_SERVER);
 
 include CORE.'/common/function.php';
 include CORE.'/imooc.php';
